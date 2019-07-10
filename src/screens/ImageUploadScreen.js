2702088ca080d6b90e87d03a1de3b4329b1e5a12
jhyang2362 <jhyang2362@naver.com>
2019-07-10 17:45:45 +0900

    마커 이미지 업로드 구현 중

--- src/screens/ImageUploadScreen.js
+++ src/screens/ImageUploadScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, Text, Image, Button, Platform } from 'react-native'
+import { View, Text, Image, Button, Platform, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native'
 import ImagePicker from 'react-native-image-picker'
 import UserInfo from '../UserInfo'
-
+import Icons from "react-native-vector-icons";
+import MapView, { Marker } from 'react-native-maps';
 const createFormData = (photo, body) => {
     const data = new FormData();
   
@@ -23,6 +24,11 @@ const createFormData = (photo, body) => {
 export default class ImageUploadScreen extends React.Component {
   state = {
     photo: null,
+    location: "방문지",
+    content: "간단한 설명",
+    latitude: null,
+    longitude: null,
+    imagepicked: false
   }
 
   handleChoosephoto = () => {
@@ -31,7 +37,15 @@ export default class ImageUploadScreen extends React.Component {
     }
     ImagePicker.launchImageLibrary(options, response => {
       if (response.uri) {
-        this.setState({ photo: response })
+        this.setState({ photo: response });
+        this.setState({imagepicked: true})
+        console.log(response);
+        if(response.hasOwnProperty('latitude')){
+          this.setState({latitude: response.latitude});
+        }
+        if(response.hasOwnProperty('longitude')){
+          this.setState({longitude: response.longitude});
+        }
       }
     })
   }
@@ -65,20 +79,92 @@ handleUploadphoto = () => {
   };
 
   render() {
-    const { photo } = this.state
+    //const { photo } = this.state.photo
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {photo && (
-          <React.Fragment>
-            <Image
-                source={{ uri: photo.uri }}
-                style={{ width: 300, height: 300 }}
-            />
-            <Button title="Upload" onPress={this.handleUploadphoto} />
-          </React.Fragment>
-        )}
-        <Button title="Choose photo" onPress={this.handleChoosephoto} />
-      </View>
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false} >
+        {this.state.photo ? (
+        <React.Fragment>
+          <Image
+              source={{ uri: this.state.photo.uri }}
+              style={{ height: 300 }}
+          />
+        </React.Fragment>
+        ): (
+        <View style={styles.imageContainer}>
+          <Button title="대표사진 선택" onPress={this.handleChoosephoto}/>
+        </View>)}
+        {
+          
+          this.state.imagepicked&&(
+            (this.state.latitude!==null&&this.state.longitude!==null)?(
+           <Text style={styles.locationKnow}> 위치정보 확인</Text>
+          ):(
+           <View style={styles.locationContainer}>
+            <Text style={styles.locationUnknown}> 위치정보 확인 불가</Text>
+            <TouchableOpacity style={styles.buttonContainer}>
+               <Text>수동 설정</Text>
+            </TouchableOpacity>
+           </View>
+           
+          ))
+        }
+        <TextInput style={styles.input} onChangeText={(location) => this.setState({location})} value={this.state.location} />
+        <TextInput style={styles.input} onChangeText={(content) => this.setState({content})} value={this.state.content} />
+        <TouchableOpacity style={styles.buttonContainer} onPress={this.handleUploadphoto} >
+          <Text>확인</Text>
+        </TouchableOpacity>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,  
+  },
+  input: {
+    height: 40, 
+    margin: 10,
+    borderColor: 'gray', 
+    borderWidth: 1
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40, 
+    margin: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    borderColor: '#000',
+    borderWidth: 1
+  },
+  imageContainer: {
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'gray',
+    borderRadius: 5,
+    borderWidth: 3,
+    borderStyle: 'dashed'
+  },
+  mapContainer: {
+    height: "50%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  locationContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems:"center"
+
+  },
+  locationKnow: {
+    color: "green"
+  },
+  locationUnknown: {
+    color: "red"
+  }
+});
