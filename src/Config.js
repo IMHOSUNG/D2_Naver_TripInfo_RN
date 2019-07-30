8fa1c6abb34202301007a36d93c1a4589c9d13bf
IMHOSUNG <dlaghtjd4487@naver.com>
2019-07-31 00:52:18 +0900

    불필요한 screen 제거

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const config = {
   host : "http://www.playinfo.co.kr",
   loadingImg: "",
-  defaultImg: "5d21b959ae469443cef69768"
+  defaultImg: "5d4065d16f979f1d67d4aab3"
 }
 export default config;
